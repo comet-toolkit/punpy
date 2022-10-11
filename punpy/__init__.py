32057f8373cbd94156ae4f085011b9fb2bb6748e
Pieter De Vis <pieter.de.vis@npl.co.uk>
2022-10-11 10:28:36 +0100

    change versioning

--- punpy/__init__.py
+++ punpy/__init__.py
@@ -1,7 +1,7 @@
 from punpy.digital_effects_table.measurement_function import MeasurementFunction
 from punpy.lpu.lpu_propagation import LPUPropagation
 from punpy.mc.mc_propagation import MCPropagation
-from ._version import get_versions
+from ._version import __version__
 
 # load functionality of comet_maths as this used to be in punpy, and is generally useful as part of punpy
 
@@ -11,6 +11,3 @@ __created__ = "01/03/2020"
 __maintainer__ = "Pieter De Vis"
 __email__ = "pieter.de.vis@npl.co.uk"
 __status__ = "Development"
-
-__version__ = get_versions()["version"]
-del get_versions
