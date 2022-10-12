fc7c30712550f1b7dd904f03877499404e262137
Pieter De Vis <pieter.de.vis@npl.co.uk>
2022-10-12 12:13:51 +0100

    enable imports of comet functionality from punpy

--- punpy/__init__.py
+++ punpy/__init__.py
@@ -1,9 +1,26 @@
+from comet_maths.linear_algebra.matrix_calculation import (calculate_Jacobian,
+                                                           calculate_corr,
+                                                           nearestPD_cholesky,isPD,)
+from comet_maths.linear_algebra.matrix_conversion import (calculate_flattened_corr,
+                                                          separate_flattened_corr,
+                                                          convert_corr_to_cov,
+                                                          convert_cov_to_corr,
+                                                          correlation_from_covariance,
+                                                          uncertainty_from_covariance,
+                                                          change_order_errcorr_dims,
+                                                          expand_errcorr_dims,)
+# load functionality of comet_maths as this used to be in punpy, and is generally useful as part of punpy
+from comet_maths.random.generate_sample import (generate_sample,
+                                                generate_sample_systematic,
+                                                generate_sample_random,
+                                                generate_sample_cov,
+                                                correlate_sample_corr,)
+
 from punpy.digital_effects_table.measurement_function import MeasurementFunction
 from punpy.lpu.lpu_propagation import LPUPropagation
 from punpy.mc.mc_propagation import MCPropagation
+from punpy.utilities.correlation_forms import *
 from ._version import __version__
-
-# load functionality of comet_maths as this used to be in punpy, and is generally useful as part of punpy
 
 """___Authorship___"""
 __author__ = "Pieter De Vis"
