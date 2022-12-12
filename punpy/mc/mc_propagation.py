c6570b7622c51278176da7144ce240494f71b9c0
Pieter De Vis <pieter.de.vis@npl.co.uk>
2022-12-12 11:53:37 +0000

    improve handling for output_vars improve docs

--- punpy/mc/mc_propagation.py
+++ punpy/mc/mc_propagation.py
@@ -1410,7 +1410,7 @@ class MCPropagation:
                 MC_x2[i] = [MC_x[j][index, ...] for j in range(len(MC_x))]
             MC_y = self.pool.starmap(func, MC_x2)
 
-        print(MC_y)
+        print(MC_x2)
         print([np.isfinite(MC_y[i]) for i in range(len(indices))])
         if output_vars==1:
             if allow_some_nans:
